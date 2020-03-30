d8ddada23d83e44e050923679c66820ca28032a6
Valeriia-Rez <Valeria09122016@gmail.com>
2020-03-30 21:15:27 +0300

    Fixed bugs and get quotes

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import "regenerator-runtime/runtime";
+import "../scss/main.scss";
+
+import getResults from "./model/modelController";
+
+getResults();
--- src/js/model/modelController.js
+++ src/js/model/modelController.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-async function getResults() {
-    const res = await axios("https://150000-quotes.p.rapidapi.com/random?key=5adac89164msh5f363b79c62ad6ep1d5d53jsn6598a13827c2");
+export default async function getResults() {
+    const res = await axios("https://type.fit/api/quotes");
     console.log(res);
-
-
 }
-
-getResults();
