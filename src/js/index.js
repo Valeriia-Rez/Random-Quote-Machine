c34bdc1785993d030e63556a7ef7742214c709e4
Valeriia-Rez <Valeria09122016@gmail.com>
2020-03-31 13:03:06 +0300

    Add twitter button functionality

--- src/js/index.js
+++ src/js/index.js
@@ -9,10 +9,14 @@ const view = new viewController();
 
 const randomQuote = async() => {
     await model.getQuote();
+
     if (model.quote && model.author) {
         view.displayQuote(model.quote);
         view.displayAuthor(model.author);
     }
+
+    const twitterButton = document.querySelector("[data-selector='tweet']");
+    twitterButton.setAttribute('href', `https://twitter.com/intent/tweet/?text=${model.quote}${model.author}`);
 }
 
 document.querySelector("[data-selector='button']").addEventListener("click", randomQuote);
--- src/js/model/modelController.js
+++ src/js/model/modelController.js
@@ -10,11 +10,9 @@ export default class modelController {
         try {
             const res = await axios("https://type.fit/api/quotes");
             this.result = res.data;
-            console.log(this.result);
             this.id = Math.floor(Math.random() * this.result.length + 1);
             this.quote = this.result[this.id].text;
             this.author = this.result[this.id].author;
-            console.log(this.id, this.quote, this.author);
         } catch (error) {
             alert(error);
         }
